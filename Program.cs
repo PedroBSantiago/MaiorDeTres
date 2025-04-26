0f2348fd2325d000398856a93a16a4044f452ba2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 22:44:14 -0300

    pom

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+double a1 ,a2, a3, a4, a5, a6, a7, a8, a9, a10;
+
+Console.Write("digite dez numeros : ");
+a1 = Convert.ToDouble(Console.ReadLine())!;
+a2 = Convert.ToDouble(Console.ReadLine())!;
+a3 = Convert.ToDouble(Console.ReadLine())!;
+a4 = Convert.ToDouble(Console.ReadLine())!;
+a5 = Convert.ToDouble(Console.ReadLine())!;
+a6 = Convert.ToDouble(Console.ReadLine())!;
+a7 = Convert.ToDouble(Console.ReadLine())!;
+a8 = Convert.ToDouble(Console.ReadLine())!;
+a9 = Convert.ToDouble(Console.ReadLine())!;
+a10 = Convert.ToDouble(Console.ReadLine())!;
+
+if ()
+{ 
+
+    Console.WriteLine("");
+
+
+
+
+}
+else
+{
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
